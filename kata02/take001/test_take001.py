77cec2ba47533e69a9963ecb1ef8fd1e1be59c6e
cielak <4882202+cielak@users.noreply.github.com>
2021-03-29 00:08:10 +0200

    Kata02 Take001 Added test with two list items

--- kata02/take001/test_take001.py
+++ kata02/take001/test_take001.py
@@ -5,5 +5,9 @@ class TestBinarySearch:
     def test_finds_single_item(self):
         assert karate_chop(5, [5]) == 0
 
+    def test_finds_one_of_two(self):
+        assert karate_chop(1, [1, 2]) == 0
+        assert karate_chop(1, [-2, 1]) == 1
+
     def test_finds_middle_item(self):
         assert karate_chop(4, [1, 4, 8]) == 1
