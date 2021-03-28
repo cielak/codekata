9cee5a63daea551f7318ef0cf39ac5482829cfb9
cielak <4882202+cielak@users.noreply.github.com>
2021-03-28 23:49:38 +0200

    Kata02 Take001 Add test for middle item

--- kata02/take001/test_take001.py
+++ kata02/take001/test_take001.py
@@ -4,3 +4,6 @@ from take001 import karate_chop
 class TestBinarySearch:
     def test_finds_single_item(self):
         assert karate_chop(5, [5]) == 0
+
+    def test_finds_middle_item(self):
+        assert karate_chop(4, [1, 4, 8]) == 1
