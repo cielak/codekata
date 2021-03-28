6783c89c032f2461dfecc0dc746b9eda489758de
cielak <4882202+cielak@users.noreply.github.com>
2021-03-29 01:00:15 +0200

    Kata02 Take001 Solved second two items test

--- kata02/take001/take001.py
+++ kata02/take001/take001.py
@@ -1,8 +1,11 @@
 from typing import List
 
 
-def karate_chop(what: int, where: List[int]) -> int:
-    cutting_position = int((len(where) - 1) / 2)
-    value_at_cut = where[cutting_position]
+def karate_chop(what: int, items: List[int]) -> int:
+    cutting_position = int((len(items) - 1) / 2)
+    value_at_cut = items[cutting_position]
     if value_at_cut == what:
         return cutting_position
+    if value_at_cut < what:
+        lower_range_boundary = cutting_position + 1
+        return lower_range_boundary + karate_chop(what, items[lower_range_boundary:])
