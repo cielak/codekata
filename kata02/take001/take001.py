12367526d95defe3e68119ad82affe5f2399620c
cielak <4882202+cielak@users.noreply.github.com>
2021-03-29 00:43:17 +0200

    Kata02 Take001 First step to solving second two items test

--- kata02/take001/take001.py
+++ kata02/take001/take001.py
@@ -2,4 +2,7 @@ from typing import List
 
 
 def karate_chop(what: int, where: List[int]) -> int:
-    return int((len(where) - 1) / 2)
+    cutting_position = int((len(where) - 1) / 2)
+    value_at_cut = where[cutting_position]
+    if value_at_cut == what:
+        return cutting_position
