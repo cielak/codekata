f6313769881a3aeadc42349604983103d5bf6630
cielak <4882202+cielak@users.noreply.github.com>
2021-03-29 01:15:22 +0200

    Kata02 Take001 Changed ifs to elifs

--- kata02/take001/take001.py
+++ kata02/take001/take001.py
@@ -6,9 +6,9 @@ def karate_chop(what: int, items: List[int]) -> int:
     value_at_cut = items[cutting_position]
     if value_at_cut == what:
         return cutting_position
-    if value_at_cut < what:
+    elif value_at_cut < what:
         lower_range_boundary = cutting_position + 1
         return lower_range_boundary + karate_chop(what, items[lower_range_boundary:])
-    if value_at_cut > what:
+    elif value_at_cut > what:
         upper_range_boundary = cutting_position
         return karate_chop(what, items[:upper_range_boundary])
