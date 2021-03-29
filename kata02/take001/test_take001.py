e66502150f1d8e315540590fc86be241be98aea6
cielak <4882202+cielak@users.noreply.github.com>
2021-03-29 02:38:52 +0200

    Kata02 Take001 Added bigger tests with bigger lists

--- kata02/take001/test_take001.py
+++ kata02/take001/test_take001.py
@@ -1,3 +1,5 @@
+import pytest
+
 from take001 import karate_chop
 
 
@@ -19,3 +21,17 @@ class TestBinarySearch:
 
     def test_finds_third_of_three(self):
         assert karate_chop(6, [2, 4, 6]) == 2
+
+    @pytest.mark.parametrize(
+        "item_to_find, item_position",
+        ((12, 0), (24, 1), (35, 2), (43, 3), (55, 4), (63, 5), (77, 6), (80, 7)),
+    )
+    def test_finds_one_of_eight(self, item_to_find, item_position):
+        assert (
+            karate_chop(item_to_find, [12, 24, 35, 43, 55, 63, 77, 80]) == item_position
+        )
+
+    @pytest.mark.skip(reason="big range crashes")
+    @pytest.mark.parametrize("item", (0, 1, 50000000, 100000000))
+    def test_finds_in_big_range(self, item):
+        assert karate_chop(item, list(range(100000001))) == item
